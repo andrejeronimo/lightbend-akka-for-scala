ec6c7446bd754a0003630462ede09460dd45bc39
André Jerónimo <andre.jeronimo.93@gmail.com>
2020-07-13 22:05:52 +0100

    Exercise 18: Modify Behaviour - completed

--- exercises/src/main/scala/com/lightbend/training/coffeehouse/Barista.scala
+++ exercises/src/main/scala/com/lightbend/training/coffeehouse/Barista.scala
@@ -1,6 +1,6 @@
 package com.lightbend.training.coffeehouse
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, Stash, Timers}
 
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Random
@@ -14,13 +14,28 @@ object Barista {
   }
 }
 
-class Barista(prepareCoffeeDuration: FiniteDuration, accuracy: Int) extends Actor with ActorLogging {
+class Barista(prepareCoffeeDuration: FiniteDuration, accuracy: Int) extends Actor with ActorLogging
+  with Timers with Stash {
   import Barista._
 
-  def receive(): Receive = {
+  /** The default behaviour is: ready behaviour */
+  override def receive: Receive = ready
+
+  /** Ready behaviour - When barista is ready to receive another request */
+  private def ready: Receive = {
     case PrepareCoffee(coffee, guest) =>
-      busy(prepareCoffeeDuration)
-      sender() ! CoffeePrepared(pickCoffee(coffee), guest)
+      timers.startSingleTimer("coffee-prepared", CoffeePrepared(pickCoffee(coffee), guest), prepareCoffeeDuration)
+      context.become(busy(sender()))
+  }
+
+  /** Busy behaviour - When barista is not ready to process a new request */
+  private def busy(waiter: ActorRef): Receive = {
+    case coffeePrepared: CoffeePrepared =>
+      waiter ! coffeePrepared
+      unstashAll()
+      context.become(ready)
+    case _ =>
+      stash()
   }
 
   /** Randomly picks a coffee based on the barista accuracy */
